b0796f546900465a1729ad1ac68d5ed5b2fae4df
javier <javier.mediavilla.vegas@code2run.net>
2020-02-29 19:21:58 +0100

    SAM-0 - Adds log statements.

--- src/main/java/com/coding2go/DistributionGenerator.java
+++ src/main/java/com/coding2go/DistributionGenerator.java
@@ -2,31 +2,38 @@ package com.coding2go;
 
 import com.coding2go.common.RealHelper;
 import com.coding2go.exceptions.DistributionException;
+import org.apache.log4j.Logger;
 
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DistributionGenerator {
+    private static final Logger logger = Logger.getLogger(DistributionGenerator.class);
     private static final RealHelper realHelper = new RealHelper();
     private final int numClasses;
 
     public DistributionGenerator(int numClasses) {
         if(numClasses <= 0) {
-            throw new InvalidParameterException();
+            throw new InvalidParameterException("The number of classes must be possitive not " + numClasses + ".");
         }
+
+        logger.info("Created a distribution generation with " + numClasses + " classes.");
 
         this.numClasses = numClasses;
     }
 
     public List<Double> generate() {
+        logger.info("Generating a distribution with " + numClasses + " classes.");
         List<Double> distribution = new ArrayList<>();
 
         for(int i = 0; i < numClasses; i++) {
             distribution.add(getDistribution(i));
         }
+        logger.info("Generated distribution: " + distribution);
 
         check(distribution);
+        logger.info("Distribution successfully generated.");
 
         return distribution;
     }
@@ -44,20 +51,22 @@ public class DistributionGenerator {
     }
 
     private void checkDistributionSums1(List<Double> distribution) {
+        logger.info("Checking distribution sums 1.");
         double sum = 0;
         for(Double d : distribution) {
             sum += d;
         }
 
         if(!realHelper.equals(sum, 1)) {
-            throw new DistributionException("The distribution adds up to " + sum + " instead of 1.");
+            throw new DistributionException("The distribution adds up to " + sum + " instead of 1. Distribution " + distribution + ".");
         }
     }
 
     private void checkOnlyGreaterOrEqualsThan0(List<Double> distribution) {
+        logger.info("Checking there are only natural values.");
         for(Double d : distribution) {
             if(d < 0) {
-                throw new DistributionException("There are negative values like " + d);
+                throw new DistributionException("There are negative values like " + d + " . Distribution " + distribution + ".");
             }
         }
     }
--- src/main/java/com/coding2go/Main.java
+++ src/main/java/com/coding2go/Main.java
@@ -3,6 +3,8 @@ package com.coding2go;
 import org.apache.log4j.Logger;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args){
